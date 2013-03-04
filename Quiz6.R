6cdfebf7d3de520e5bbb468ca508b47073e2c54d
ateucher <andy.teucher@gmail.com>
2013-03-04 12:19:56 -0800

    Started Quiz 6

---
+++ Quiz6.R
@@ -0,0 +1,7 @@
+## Question 3:
+library(ElemStatLearn)
+data(SAheart)
+set.seed(8484)
+train = sample(1:dim(SAheart)[1],size=dim(SAheart)[1]/2,replace=F)
+trainSA = SAheart[train,]
+testSA = SAheart[-train,]
